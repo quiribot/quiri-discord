38fa7e9492bbb48e7340a16dbadaa773ed5685f2
eye-sigil <ustin.a.h+github@gmail.com>
2018-03-06 17:21:58 -0500

    Some attempts at a base lib for quiri

--- lib/quiri_discord/quiri.ex
+++ lib/quiri_discord/quiri.ex
@@ -2,8 +2,13 @@ defmodule Quiri do
   use HTTPoision.Base
 
   def search(term, user) do
-    get("/search").body[[term: term, user: user.id]]
+    get("/search").body[[form: [term: term, user: user.id]]] do
+      {:ok, response} -> response,
+      {:error, reason} -> "Error"
+    end
   end
 
-  def query()
+  def query() do
+    get("/query")
+  end
 end
